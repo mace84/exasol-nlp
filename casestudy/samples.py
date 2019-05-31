a518ba9ff5bed534ef4a47941e1000f959ae74c2
Matthias Orlowski <matthias.orlowski@areto.de>
2019-05-31 08:30:05 +0200

    Create file path for storing data using os path join.

--- casestudy/samples.py
+++ casestudy/samples.py
@@ -6,12 +6,16 @@ writes the remaining data to sample0.
 
 """
 
+import os
 import pandas as pd
 
 num_small_files = 10
 small_file_nrows = 100000
 
-df = pd.read_csv('../data/steam.csv',
+
+data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                        os.pardir, 'data')
+df = pd.read_csv(os.path.join(data_dir, 'steam.csv'),
                  names=['game_id', 'review', 'sentiment', 'helpful'])
 
 df['review_id'] = list(range(1, df.shape[0] + 1))
@@ -21,11 +25,11 @@ df = df[['review_id', 'game_id', 'review', 'review_length', 'sentiment', 'helpfu
 
 for i in range(num_small_files):
     sample = df.sample(small_file_nrows, random_state=4711 + i)
-    sample.to_csv(f'./data/reviews.sample{i+1}.csv',
+    sample.to_csv(os.path.join(data_dir, f'reviews.sample{i+1}.csv'),
                   index=False,
                   encoding='utf-8')
     df = df[~df.review_id.isin(sample.review_id)]
 
-df.to_csv(f'./data/reviews.sample0.csv',
+df.to_csv(os.path.join(data_dir, 'reviews.sample0.csv'),
           index=False,
           encoding='utf-8')
